e99728055eedab9a47152bba93e0a8167e0827e1
Yun Liu <109399305+yuuuuuuunliu@users.noreply.github.com>
2023-06-07 18:12:45 -0700

    Update Sonar_1D.m

--- Sensor/Sonar_1D.m
+++ Sensor/Sonar_1D.m
@@ -1,6 +1,6 @@
-%clear; clc; clf; close all;
-%a = arduino("/dev/tty.usbserial-AQ02T0FB", "Nano3", "Libraries", 'Ultrasonic');
-%ultrasonicObj = ultrasonic(a, "D3", "D2");
+clear; clc; clf; close all;
+a = arduino("/dev/tty.usbserial-AQ02T0FB", "Nano3", "Libraries", 'Ultrasonic');
+ultrasonicObj = ultrasonic(a, "D3", "D2");
 
 distance = [];
 t_max = 100;
