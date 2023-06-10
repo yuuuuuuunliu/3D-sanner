93e6d96bd4c826573f9238029d4d02c18b7cc87b
Yun Liu <109399305+yuuuuuuunliu@users.noreply.github.com>
2023-06-09 21:50:25 -0700

    Update Sonar_1D.m

--- Sensor/Sonar_1D.m
+++ Sensor/Sonar_1D.m
@@ -1,11 +1,13 @@
 clear; clc; clf; close all;
 a = arduino("/dev/tty.usbserial-AQ02T0FB", "Nano3", "Libraries", 'Ultrasonic');
+#Creat an arduino object
 ultrasonicObj = ultrasonic(a, "D3", "D2");
+#Creat an ultrasonic object
 
 distance = [];
-t_max = 100;
+t_max = 100; #define the maxmimum number of trials
 for trial = 1 : 1 : t_max
-    distance = [distance readDistance(ultrasonicObj)];
+    distance = [distance readDistance(ultrasonicObj)]; #measure distance and add the measurement to the end of the array
     pause (0.04);
 end
 mean(distance)
