fe788686cf0c91134c1af8878cf84184d16efb68
Yun Liu <109399305+yuuuuuuunliu@users.noreply.github.com>
2023-06-09 21:52:28 -0700

    Update Sonar_2D.m

--- Sensor/Sonar_2D.m
+++ Sensor/Sonar_2D.m
@@ -1,6 +1,9 @@
 clear; clc; clf; close all;
 a = arduino("/dev/tty.usbserial-AQ02T0FB", "Nano3", "Libraries", 'Ultrasonic');
+#Creat an arduino object
 ultrasonicObj = ultrasonic(a, "D3", "D2");
+#Creat an ultrasonic object
+#Trig pin is connected to D3 of Aduino Nano and Echo pin is connected to D2
 
 distance = [];
 t_max = 10;
@@ -11,11 +14,11 @@ for trial = 0 : step: t_max
 end
 
 trial = floor(t_max/step)+1;
-velocity = 0.1;
+velocity = 0.1; #The velocity of which the sensor move
 t = 0 : step: step*(trial-1);
 x = t.*velocity;
 
-smoothData = smoothdata(distance, "movmedian", 100);
+smoothData = smoothdata(distance, "movmedian", 50);
 plot(x,smoothData)
 xlabel('x/dm') 
 ylabel('y/dm') 
