f42496f24ffd434bf65351a72f6ad274022471df
Yun Liu <109399305+yuuuuuuunliu@users.noreply.github.com>
2023-06-09 21:51:31 -0700

    Update Sonar_1D.m

--- Sensor/Sonar_1D.m
+++ Sensor/Sonar_1D.m
@@ -3,6 +3,7 @@ a = arduino("/dev/tty.usbserial-AQ02T0FB", "Nano3", "Libraries", 'Ultrasonic');
 #Creat an arduino object
 ultrasonicObj = ultrasonic(a, "D3", "D2");
 #Creat an ultrasonic object
+#Trig pin is connected to D3 of Aduino Nano and Echo pin is connected to D2
 
 distance = [];
 t_max = 100; #define the maxmimum number of trials
