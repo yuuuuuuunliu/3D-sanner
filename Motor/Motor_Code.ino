58e1a7b7f43e597cf8cef41bfea9a80e0f0d3a4a
Yun Liu <109399305+yuuuuuuunliu@users.noreply.github.com>
2023-06-09 21:47:51 -0700

    Update Motor_Code.ino

--- Motor/Motor_Code.ino
+++ Motor/Motor_Code.ino
@@ -2,7 +2,8 @@
 
 const int stepsPerRevolution = 2038;
 
-Stepper myStepper = Stepper(stepsPerRevolution, 8, 10, 9, 11);
+Stepper myStepper = Stepper(stepsPerRevolution, 8, 10, 9, 11); 
+//Create a stepper object with its 4 pins connecting to D8, D10, D9, and D11 of Aduino Uno, respectivly
 
 void setup() {
     //leave it blank
@@ -10,7 +11,7 @@ void setup() {
 
 void loop() {
  while (true) {
-  myStepper.setSpeed(15);
+  myStepper.setSpeed(15); //The speed of motor is set to 15 RPM
   myStepper.step(stepsPerRevolution); //Lowering the platform
   //myStepper.step(-stepsPerRevolution); //Raising the platform
   }
